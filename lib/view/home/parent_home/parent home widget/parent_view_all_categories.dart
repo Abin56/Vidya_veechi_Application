b4f36035b34a42bb69f3c4a55da2e6ed71b01e22
Reshmas23 <reshmasuresh8089@gmail.com>
2024-04-29 17:27:56 +0530

    april 29 first commit --@reshma

--- lib/view/home/parent_home/parent home widget/parent_view_all_categories.dart
+++ lib/view/home/parent_home/parent home widget/parent_view_all_categories.dart
@@ -1,4 +1,7 @@
+import 'package:adaptive_ui_layout/flutter_responsive_layout.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:vidya_veechi/utils/utils.dart';
 import 'package:vidya_veechi/view/colors/colors.dart';
 
 class ParentViewAllCategories extends StatelessWidget {
@@ -63,69 +66,230 @@ class ParentViewAllCategories extends StatelessWidget {
               padding: const EdgeInsets.only(
                   top: 80, right: 20, left: 20),
               child: Column(
-                children: [
-                  Row(
-                    children: [
-                      const Text(
-                        'NOTIFICATIONS',
-                        style: TextStyle(
-                            color:adminePrimayColor,
-                               // Color.fromARGB(255, 48, 88, 86),
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold),
-                      ),
-                      Expanded(
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Container(
-                            height: 1,
-                            color:adminePrimayColor
-                            // const Color.fromARGB(255, 48, 88, 86)
-                                .withOpacity(0.1),
-                          ),
+      children: [
+        Row(
+          children: [
+            Text(
+              'NOTIFICATIONS',
+              style: TextStyle(
+                  color: const Color.fromARGB(255, 11, 2, 74),
+                  //const Color.fromARGB(255, 48, 88, 86),
+                  fontSize: 16.sp,
+                  fontWeight: FontWeight.bold),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 1.h,
+                  color: const Color.fromARGB(255, 11, 2, 74).withOpacity(0.1),
+                  // const Color.fromARGB(255, 48, 88, 86).withOpacity(0.1),
+                ),
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                showDialog(
+                  context: context,
+                  builder: (context) => AlertDialog(
+                    title: const Text(
+                      "Do you want to clear all notifications ?",
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    actions: [
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: const Text(
+                          "No",
+                          style: TextStyle(color: cblack),
                         ),
-                      )
+                      ),
+                      TextButton(
+                        onPressed: () {},
+                        child: const Text(
+                          "yes",
+                          style: TextStyle(color: cblack),
+                        ),
+                      ),
                     ],
                   ),
-                  SizedBox(
-                    height: 290,
-                    child: ListView.separated(
-                        // physics:
-                        //     const NeverScrollableScrollPhysics(),
-                        // shrinkWrap: false,
-                        itemBuilder: (context, index) {
-                          return const ListTile(
+                );
+              },
+              child: Text(
+                "Clear All",
+                style: TextStyle(color: cblack.withOpacity(0.8)),
+              ),
+            ),
+          ],
+        ),
+        SizedBox(
+          height: 290.h,
+          child: StreamBuilder(
+              stream: server
+                  .collection('AllUsersDeviceID')
+                  .doc(FirebaseAuth.instance.currentUser!.uid)
+                  .collection("Notification_Message")
+                  .snapshots(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return ListView.separated(
+                      // physics:
+                      //     const NeverScrollableScrollPhysics(),
+                      // shrinkWrap: false,
+                      itemBuilder: (context, index) {
+                        final data = snapshot.data!.docs[index].data();
+                        return InkWell(
+                          onTap: () {
+                            showModalBottomSheet(
+                              shape: const BeveledRectangleBorder(),
+                              context: context,
+                              builder: (context) {
+                                return Container(
+                                  color: Color(data['whiteshadeColor']),
+                                  child: Column(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      Container(
+                                        color: Color(data['containerColor']),
+                                        padding: const EdgeInsets.symmetric(
+                                            horizontal: 20, vertical: 20),
+                                        child: Row(
+                                          children: [
+                                            Icon(
+                                              IconData(
+                                                data['icon'],
+                                                fontFamily: 'MaterialIcons',
+                                              ),
+                                              size: 25,
+                                              color: Colors.white,
+                                            ),
+                                            const SizedBox(
+                                              width: 20,
+                                            ),
+                                            Text(
+                                              data['headerText'],
+                                              // "Holiday",
+                                              style: const TextStyle(
+                                                  color: cWhite,
+                                                  fontSize: 20,
+                                                  fontWeight: FontWeight.bold),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                      Padding(
+                                        padding: const EdgeInsets.symmetric(
+                                            horizontal: 20, vertical: 20),
+                                        child: Text(
+                                          data['messageText'],
+                                          // " Tommorow is Holiday Tommorow is Holiday Tommorow is Holiday Tommorow is Holiday",
+                                          textAlign: TextAlign.justify,
+                                          style: const TextStyle(
+                                            fontSize: 17,
+                                            color: cWhite,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                );
+                              },
+                            );
+                          },
+                          child: ListTile(
                             leading: CircleAvatar(
-                              backgroundColor: cWhite,
+                              backgroundColor: Color(data['containerColor']),
                               radius: 25,
                               child: Center(
                                 child: CircleAvatar(
                                   radius: 20,
+                                  backgroundColor:
+                                      Color(data['whiteshadeColor']),
+                                  child: Center(
+                                    child: Icon(
+                                      IconData(data['icon'],
+                                          fontFamily: 'MaterialIcons'),
+                                      color: Colors.white,
+                                    ),
+                                  ),
                                 ),
                               ),
                             ),
                             title: Text(
-                              "Holiday",
-                              style: TextStyle(
-                                  color:adminePrimayColor,
+                              data['headerText'],
+                              // "Holiday",
+                              style: const TextStyle(
+                                  color: Color.fromARGB(255, 48, 88, 86),
                                   fontSize: 18,
                                   fontWeight: FontWeight.bold),
                             ),
                             subtitle: Text(
-                              "Tommorow is Holiday",
-                              style: TextStyle(
-                                color: adminePrimayColor,
+                              data['messageText'],
+                              // "Tommorow is Holiday",
+                              overflow: TextOverflow.ellipsis,
+                              maxLines: 1,
+                              style: const TextStyle(
+                                color: Color.fromARGB(255, 48, 88, 86),
                               ),
                             ),
-                          );
-                        },
-                        separatorBuilder: (context, index) {
-                          return const SizedBox();
-                        },
-                        itemCount: 10),
-                  )
-                ],
-              ),
+                            trailing: GestureDetector(
+                              onTap: () {
+                                showDialog(
+                                  context: context,
+                                  builder: (context) => AlertDialog(
+                                    title: const Text(
+                                      "Do you want to remove the notification ?",
+                                      style: TextStyle(fontSize: 16),
+                                    ),
+                                    actions: [
+                                      TextButton(
+                                        onPressed: () {
+                                          Navigator.pop(context);
+                                        },
+                                        child: const Text(
+                                          "No",
+                                          style: TextStyle(color: cblack),
+                                        ),
+                                      ),
+                                      TextButton(
+                                        onPressed: () {},
+                                        child: const Text(
+                                          "yes",
+                                          style: TextStyle(color: cblack),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                );
+                              },
+                              child: Icon(
+                                Icons.close,
+                                size: 20,
+                                color: cblack.withOpacity(0.8),
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                      separatorBuilder: (context, index) {
+                        return const SizedBox();
+                      },
+                      itemCount: snapshot.data!.docs.length);
+                } else if (snapshot.data == null) {
+                  return Text(
+                    "NO Notifications",
+                    style:
+                        TextStyle(fontSize: 20, color: cblack.withOpacity(0.5)),
+                  );
+                } else {
+                  return circularProgressIndicatotWidget;
+                }
+              }),
+        )
+      ],
+    )
             ),
           ],
         ),
