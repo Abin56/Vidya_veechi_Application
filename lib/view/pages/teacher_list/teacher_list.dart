39dca94d3c0f5febeb32bdfbec85ff216b5f8058
Reshmas23 <reshmasuresh8089@gmail.com>
2024-05-07 15:08:15 +0530

    may 7 3:08 commit --@reshma

--- lib/view/pages/teacher_list/teacher_list.dart
+++ lib/view/pages/teacher_list/teacher_list.dart
@@ -1,13 +1,13 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:vidya_veechi/utils/utils.dart';
 import 'package:vidya_veechi/view/widgets/appbar_color/appbar_clr.dart';
-import 'package:flutter/material.dart';
-import 'package:adaptive_ui_layout/flutter_responsive_layout.dart';
-import 'package:get/get.dart';
 
 import '../../../controllers/teacher_list/teacher_list_controller.dart';
 import '../../colors/colors.dart';
 import '../../constant/sizes/sizes.dart';
-import '../../widgets/fonts/google_poppins.dart';
 
 class TeacherSubjectWiseList extends StatelessWidget {
   final String navValue;
@@ -32,7 +32,7 @@ class TeacherSubjectWiseList extends StatelessWidget {
             if (snapshot.connectionState == ConnectionState.waiting) {
               return circularProgressIndicatotWidget;
             }
-            if (snapshot.hasData && snapshot.data!.isNotEmpty) {
+            if (snapshot.hasData) {
               return Column(
                 children: [
                   kHeight10,
@@ -43,108 +43,115 @@ class TeacherSubjectWiseList extends StatelessWidget {
                           return kHeight10;
                         }),
                         itemBuilder: (BuildContext context, int index) {
+                          log(snapshot.data!.length.toString());
                           return Column(
                             children: [
                               Padding(
                                 padding:
                                     const EdgeInsets.only(left: 10, right: 10),
                                 child: Container(
-                                  decoration: const BoxDecoration(),
+                                  height: 100,
+                                  decoration: const BoxDecoration(
+                                  color: Colors.amber,
+
+                                  ),
                                   child: FutureBuilder(
                                       future:
                                           teacherListController.getTeacherData(
                                               snapshot.data![index].teacherId),
                                       builder: (context, snap) {
+                                        
                                         if (snap.connectionState ==
                                             ConnectionState.waiting) {
                                           return circularProgressIndicatotWidget;
                                         }
                                         if (snap.hasData) {
-                                          return ListTile(
-                                            shape: const BeveledRectangleBorder(
-                                                side: BorderSide(
-                                                    color: Colors.grey,
-                                                    width: 0.2)),
-                                            leading: CircleAvatar(
-                                              radius: 25,
-                                              backgroundImage: snap
-                                                      .data!.imageUrl!.isEmpty
-                                                  ? const NetworkImage(
-                                                      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHQYMHHvI6NnOaZp9iU5Wa1MOhAwxzE0oRRw&usqp=CAU")
-                                                  : NetworkImage(
-                                                      snap.data!.imageUrl!),
-                                              onBackgroundImageError:
-                                                  (exception, stackTrace) {},
-                                            ),
-                                            title: Padding(
-                                              padding:
-                                                  EdgeInsets.only(top: 10.h),
-                                              child: GooglePoppinsWidgets(
-                                                text: snap.data!.teacherName ??
-                                                    " ",
-                                                fontsize: 21.h,
-                                                fontWeight: FontWeight.w500,
-                                              ),
-                                            ),
-                                            subtitle: Column(
-                                              crossAxisAlignment:
-                                                  CrossAxisAlignment.start,
-                                              children: [
-                                                Padding(
-                                                  padding: EdgeInsets.only(
-                                                      top: 10.h),
-                                                  child: GooglePoppinsWidgets(
-                                                    text: snap.data!
-                                                            .teacherEmail ??
-                                                        " ",
-                                                    fontsize: 14.h,
-                                                    fontWeight: FontWeight.bold,
-                                                    color: Colors.blue,
-                                                  ),
-                                                ),
-                                                Padding(
-                                                  padding: EdgeInsets.only(
-                                                      top: 10.h),
-                                                  child: GooglePoppinsWidgets(
-                                                    text: snap.data!
-                                                            .teacherPhNo ??
-                                                        " ",
-                                                    fontsize: 14.h,
-                                                    fontWeight: FontWeight.bold,
-                                                  ),
-                                                ),
-                                                Padding(
-                                                  padding: EdgeInsets.only(
-                                                      top: 10.h),
-                                                  child: Row(
-                                                    mainAxisAlignment:
-                                                        MainAxisAlignment.end,
-                                                    children: [
-                                                      GooglePoppinsWidgets(
-                                                        text: "Subject :".tr,
-                                                        fontsize: 14.h,
-                                                        fontWeight:
-                                                            FontWeight.bold,
-                                                      ),
-                                                      Flexible(
-                                                        child:
-                                                            GooglePoppinsWidgets(
-                                                          text: snapshot
-                                                              .data![index]
-                                                              .subjectName,
-                                                          fontsize: 14.h,
-                                                          fontWeight:
-                                                              FontWeight.bold,
-                                                          color:
-                                                              adminePrimayColor,
-                                                        ),
-                                                      ),
-                                                    ],
-                                                  ),
-                                                ),
-                                              ],
-                                            ),
-                                          );
+                                          return  Container(color: cblue,width: 300,height: 100,);
+                                          // return ListTile(
+                                          //   shape: const BeveledRectangleBorder(
+                                          //       side: BorderSide(
+                                          //           color: Colors.grey,
+                                          //           width: 0.2)),
+                                          //   leading: CircleAvatar(
+                                          //     radius: 25,
+                                          //     backgroundImage: snap
+                                          //             .data!.imageUrl!.isEmpty
+                                          //         ? const NetworkImage(
+                                          //             "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHQYMHHvI6NnOaZp9iU5Wa1MOhAwxzE0oRRw&usqp=CAU")
+                                          //         : NetworkImage(
+                                          //             snap.data!.imageUrl!),
+                                          //     onBackgroundImageError:
+                                          //         (exception, stackTrace) {},
+                                          //   ),
+                                          //   title: Padding(
+                                          //     padding:
+                                          //         EdgeInsets.only(top: 10.h),
+                                          //     child: GooglePoppinsWidgets(
+                                          //       text: snap.data!.teacherName ??
+                                          //           " ",
+                                          //       fontsize: 21.h,
+                                          //       fontWeight: FontWeight.w500,
+                                          //     ),
+                                          //   ),
+                                          //   subtitle: Column(
+                                          //     crossAxisAlignment:
+                                          //         CrossAxisAlignment.start,
+                                          //     children: [
+                                          //       Padding(
+                                          //         padding: EdgeInsets.only(
+                                          //             top: 10.h),
+                                          //         child: GooglePoppinsWidgets(
+                                          //           text: snap.data!
+                                          //                   .teacherEmail ??
+                                          //               " ",
+                                          //           fontsize: 14.h,
+                                          //           fontWeight: FontWeight.bold,
+                                          //           color: Colors.blue,
+                                          //         ),
+                                          //       ),
+                                          //       Padding(
+                                          //         padding: EdgeInsets.only(
+                                          //             top: 10.h),
+                                          //         child: GooglePoppinsWidgets(
+                                          //           text: snap.data!
+                                          //                   .teacherPhNo ??
+                                          //               " ",
+                                          //           fontsize: 14.h,
+                                          //           fontWeight: FontWeight.bold,
+                                          //         ),
+                                          //       ),
+                                          //       Padding(
+                                          //         padding: EdgeInsets.only(
+                                          //             top: 10.h),
+                                          //         child: Row(
+                                          //           mainAxisAlignment:
+                                          //               MainAxisAlignment.end,
+                                          //           children: [
+                                          //             GooglePoppinsWidgets(
+                                          //               text: "Subject :".tr,
+                                          //               fontsize: 14.h,
+                                          //               fontWeight:
+                                          //                   FontWeight.bold,
+                                          //             ),
+                                          //             Flexible(
+                                          //               child:
+                                          //                   GooglePoppinsWidgets(
+                                          //                 text: snapshot
+                                          //                     .data![index].
+                                          //                     subjectName,
+                                          //                 fontsize: 14.h,
+                                          //                 fontWeight:
+                                          //                     FontWeight.bold,
+                                          //                 color:
+                                          //                     adminePrimayColor,
+                                          //               ),
+                                          //             ),
+                                          //           ],
+                                          //         ),
+                                          //       ),
+                                          //     ],
+                                          //   ),
+                                          // );
                                         } else {
                                           return kHeight;
                                         }
