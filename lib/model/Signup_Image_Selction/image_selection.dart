7b16dfed6577fa87d2f379482eea2c17a1ce761c
Reshmas23 <reshmasuresh8089@gmail.com>
2024-05-09 16:58:05 +0530

    may 9 4:57 commit --@reshma

--- lib/model/Signup_Image_Selction/image_selection.dart
+++ lib/model/Signup_Image_Selction/image_selection.dart
@@ -1,8 +1,8 @@
 // ignore_for_file: override_on_non_overriding_member, unused_local_variable
 
-import 'package:vidya_veechi/utils/utils.dart';
 import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:vidya_veechi/utils/utils.dart';
 
 class GetImage extends GetxController {
   RxString pickedImage = RxString("");
@@ -12,6 +12,7 @@ class GetImage extends GetxController {
       final image = await ImagePicker().pickImage(source: source);
       if (image != null) {
         pickedImage.value = image.path;
+        print(image.path);
       }
     } catch (e) {
       showToast(msg: "Failed to Pick Image");
